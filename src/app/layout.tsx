e55d34ecd78203e2ec099a51c6e30bb1735b1830
Ivan <ivanglzr01@gmail.com>
2024-08-10 20:26:14 +0200

    Metadata added into layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,23 @@ import "@/css/index.css";
 
 import { poppins } from "@/css/fonts";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "GymApp",
+  description:
+    "An amazing app to save all of your trainings in the gym! You can also create new exercises and save them",
+  keywords: "gym, training, exercise",
+  robots: "index, follow",
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="es">
+    <html lang="en">
       <head>
         <title>GymApp</title>
       </head>
