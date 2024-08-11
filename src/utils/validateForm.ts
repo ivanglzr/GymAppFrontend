e52047a1a7f796a9a38d52b1d9674eab2dc074e7
Ivan <ivanglzr01@gmail.com>
2024-08-11 22:49:56 +0200

    fix: error handling in validation functions

--- src/utils/validateForm.ts
+++ src/utils/validateForm.ts
@@ -15,12 +15,14 @@ export function validateSignInForm({
   weight,
   height,
 }: SignInInterface): string | null {
+  let error: string | null = null;
+
   if (!name || !email || !password || !age || !weight || !height) {
-    return "There is data missing";
+    error = "There is data missing";
   }
 
   if (name.length < 3) {
-    return "Name is too short";
+    error = "Name is too short";
   }
 
   if (
@@ -28,26 +30,28 @@ export function validateSignInForm({
     email.indexOf("@") === -1 ||
     email.indexOf(".") === -1
   ) {
-    return "Email isn't valid";
+    error = "Email isn't valid";
   }
 
   if (password.length < 8) {
-    return "Password is too short";
+    error = "Password is too short";
   }
 
   if (age < 12) {
-    return "You're too young";
+    error = "You're too young";
   }
 
-  return null;
+  return error;
 }
 
 export function validateLogInForm({
   email,
   password,
 }: LogInInterface): string | null {
+  let error: string | null = null;
+
   if (!email || !password) {
-    return "There is data missing";
+    error = "There is data missing";
   }
 
   if (
@@ -55,60 +59,62 @@ export function validateLogInForm({
     email.indexOf("@") === -1 ||
     email.indexOf(".") === -1
   ) {
-    return "Email isn't valid";
+    error = "Email isn't valid";
   }
 
   if (password.length < 8) {
-    return "Password is too short";
+    error = "Password is too short";
   }
 
-  return null;
+  return error;
 }
 
 export function validateTrainingForm(training: Training): string | null {
+  let error: string | null = null;
+
   // Validar la fecha
   if (!(training.date instanceof Date) || isNaN(training.date.getTime())) {
-    return "La fecha es inválida.";
+    error = "La fecha es inválida.";
   }
 
   // Validar la duración
   if (typeof training.duration !== "number" || training.duration <= 0) {
-    return "La duración debe ser un número positivo.";
+    error = "La duración debe ser un número positivo.";
   }
 
   // Validar ejercicios
   if (!Array.isArray(training.exercises) || training.exercises.length === 0) {
-    return "Debe haber al menos un ejercicio.";
+    error = "Debe haber al menos un ejercicio.";
   }
 
   training.exercises.forEach((exercise, index) => {
     // Validar nombre del ejercicio
     if (typeof exercise.name !== "string" || exercise.name.trim() === "") {
-      return `El nombre del ejercicio ${index + 1} es inválido.`;
+      error = `El nombre del ejercicio ${index + 1} es inválido.`;
     }
 
     // Validar sets del ejercicio
     if (!Array.isArray(exercise.sets) || exercise.sets.length === 0) {
-      return `El ejercicio ${index + 1} debe tener al menos un set.`;
+      error = `El ejercicio ${index + 1} debe tener al menos un set.`;
     }
 
     exercise.sets.forEach((set, setIndex) => {
       // Validar peso del set
       if (typeof set.weight !== "number" || set.weight < 0) {
-        return `El peso del set ${setIndex + 1} del ejercicio ${
+        error = `El peso del set ${setIndex + 1} del ejercicio ${
           index + 1
         } debe ser un número positivo.`;
       }
       // Validar repeticiones del set
       if (typeof set.reps !== "number" || set.reps < 0) {
-        return `El número de repeticiones del set ${
+        error = `El número de repeticiones del set ${
           setIndex + 1
         } del ejercicio ${index + 1} debe ser un número positivo.`;
       }
     });
   });
 
-  return null;
+  return error;
 }
 
 export function validateExerciseForm({
@@ -117,28 +123,30 @@ export function validateExerciseForm({
   muscle,
   equipment,
 }: UserExercise): string | null {
+  let error: string | null = null;
+
   if (!name || !muscle || !equipment) {
-    return "There is data missing";
+    error = "There is data missing";
   }
 
   if (typeof name !== "string" || name.length <= 1) {
-    return "Name isn't valid";
+    error = "Name isn't valid";
   }
 
   if (description && typeof description !== "string") {
-    return "Description isn't valid";
+    error = "Description isn't valid";
   }
 
   if (typeof muscle !== "string" || !muscularGroups.includes(muscle)) {
-    return "Muscle isn't valid";
+    error = "Muscle isn't valid";
   }
 
   if (
     typeof equipment !== "string" ||
     !exerciseEquipments.includes(equipment)
   ) {
-    return "Equipment isn't valid";
+    error = "Equipment isn't valid";
   }
 
-  return null;
+  return error;
 }
