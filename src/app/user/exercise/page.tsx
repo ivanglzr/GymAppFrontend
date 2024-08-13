7bdfce8a1ab28cb9353645dbdae0aff987ce384a
Ivan <ivanglzr01@gmail.com>
2024-08-13 22:19:17 +0200

    initial UserExercisesPage

--- src/app/user/exercise/page.tsx
+++ src/app/user/exercise/page.tsx
@@ -1,22 +1,13 @@
 "use client";
 
-import { useRef } from "react";
+import Aside from "@/components/Aside";
+import Container from "@/components/Container";
+import Exercises from "@/components/Exercises";
 
 import { useExercises } from "@/hooks/useExercises";
 
-const generateRandomKey = () => crypto.randomUUID();
-
 export default function UserExercisesPage() {
   const { exercises, error, loading } = useExercises();
-
-  const exercisesKeys = useRef<Array<string>>([]);
-
-  const getExerciseKey = (exerciseIndex: number) => {
-    if (!exercisesKeys.current[exerciseIndex]) {
-      exercisesKeys.current[exerciseIndex] = generateRandomKey();
-    }
-    return exercisesKeys.current[exerciseIndex];
-  };
 
   if (error) {
     return <h2>An error occurred</h2>;
@@ -28,15 +19,15 @@ export default function UserExercisesPage() {
 
   //TODO: Exercises UI, create Exercises component and Exercise component
   return (
-    <>
-      {exercises?.map((e, index) => (
-        <div key={getExerciseKey(index)}>
-          <h2>{e.name}</h2>
-          <p>{e.description}</p>
-          <span>{e.muscle}</span> <br />
-          <span>{e.equipment}</span>
-        </div>
-      ))}
-    </>
+    <Container
+      style={{
+        minHeight: "100vh",
+        display: "grid",
+        gridTemplateColumns: "100%",
+      }}
+    >
+      <Aside />
+      {exercises && <Exercises exercises={exercises} />}
+    </Container>
   );
 }
