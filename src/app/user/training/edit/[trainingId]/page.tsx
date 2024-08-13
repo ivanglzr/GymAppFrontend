5f97f599045fb57dc28cdf05a498da0d5495e380
Ivan <ivanglzr01@gmail.com>
2024-08-13 10:03:20 +0200

    useMemo for parsedTraining

--- src/app/user/training/edit/[trainingId]/page.tsx
+++ src/app/user/training/edit/[trainingId]/page.tsx
@@ -4,6 +4,8 @@ import TrainingForm from "@/components/TrainingForm";
 
 import { useTraining } from "@/hooks/useTraining";
 
+import { useMemo } from "react";
+
 export default function EditTrainingPage({
   params: { trainingId },
 }: {
@@ -11,26 +13,30 @@ export default function EditTrainingPage({
 }) {
   const { training, error, loading } = useTraining(trainingId);
 
+  const parsedTraining = useMemo(() => {
+    return {
+      ...training,
+      date: new Date(`${training?.date}`),
+      duration: training?.duration ?? 0, // Asignar 0 si `duration` es `undefined`
+      exercises: training?.exercises ?? [], // Asignar un array vacío si `exercises` es `undefined`
+    };
+  }, [training]);
+
   if (loading) return <h2>Loading...</h2>;
 
   if (error) {
     return <h2>Couldn&apos;t get training</h2>;
   }
 
-  if (!training) {
-    return <h2>Loading...</h2>;
-  }
-
-  const parsedTraining = {
-    ...training,
-    date: new Date(training.date),
-  };
-
   return (
-    <TrainingForm
-      isEditTraining={true}
-      trainingId={trainingId}
-      training={parsedTraining}
-    />
+    <>
+      {training && (
+        <TrainingForm
+          isEditTraining={true}
+          trainingId={trainingId}
+          training={parsedTraining}
+        />
+      )}
+    </>
   );
 }
