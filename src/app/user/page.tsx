2010bbaa5dd33c1a2de53aa03847f4c446305cbc
Ivan <ivanglzr01@gmail.com>
2024-08-13 10:07:20 +0200

    Improved UserPage

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -9,23 +9,9 @@ import UserAside from "@/components/UserAside";
 import { useUser } from "@/hooks/useUser";
 import { useTrainings } from "@/hooks/useTrainings";
 
-export default function UserPage() {
-  const { user, loading, error } = useUser();
-  const { trainings, setTrainings } = useTrainings();
+import { useMemo } from "react";
 
-  const userName = user?.name;
-
-  const numberOfTrainings = trainings.length;
-  const totalTrainingsDuration = trainings.reduce(
-    (totalDuration: number, training: Training) => {
-      return totalDuration + training.duration;
-    },
-    0
-  );
-
-  if (loading) return <h2>Loading...</h2>;
-  if (error) return <h2>Fetching trainings failed</h2>;
-
+function Container({ children }: { children: React.ReactNode }) {
   return (
     <div
       style={{
@@ -34,15 +20,38 @@ export default function UserPage() {
         gridTemplateColumns: "80% 15%",
       }}
     >
-      <>
-        <Aside />
-        <Trainings setTrainings={setTrainings} trainings={trainings} />
-        <UserAside
-          name={userName ?? ""}
-          numberOfTrainings={numberOfTrainings}
-          totalTrainingsDuration={totalTrainingsDuration}
-        />
-      </>
+      {children}
     </div>
+  );
+}
+
+export default function UserPage() {
+  const { user, loading, error } = useUser();
+  const { trainings, setTrainings } = useTrainings();
+
+  const userName = user?.name;
+
+  const numberOfTrainings = trainings.length;
+  const totalTrainingsDuration = useMemo(
+    () =>
+      trainings.reduce((totalDuration: number, training: Training) => {
+        return totalDuration + training.duration;
+      }, 0),
+    [trainings]
+  );
+
+  if (loading) return <h2>Loading...</h2>;
+  if (error) return <h2>Fetching trainings failed</h2>;
+
+  return (
+    <Container>
+      <Aside />
+      <Trainings setTrainings={setTrainings} trainings={trainings} />
+      <UserAside
+        name={userName ?? ""}
+        numberOfTrainings={numberOfTrainings}
+        totalTrainingsDuration={totalTrainingsDuration}
+      />
+    </Container>
   );
 }
