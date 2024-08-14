a2e0807784f426b5016c7c24dcdbac22db289309
Ivan <ivanglzr01@gmail.com>
2024-08-14 22:42:31 +0200

    Show message if there isn't any trainings

--- src/components/Trainings.tsx
+++ src/components/Trainings.tsx
@@ -15,50 +15,56 @@ export default function Trainings({
   trainings: Array<TrainingInterface>;
   setTrainings: (value: SetStateAction<TrainingInterface[]>) => void;
 }) {
+  const trainingsHTML = trainings.map(({ _id, date, duration, exercises }) => {
+    const parsedDate = date.toLocaleDateString("es-ES", {
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    });
+
+    const numberOfSets = exercises.reduce((totalSets, exercise) => {
+      return totalSets + exercise.sets.length;
+    }, 0);
+
+    const handleDelete = async () => {
+      const { status, message } = await deleteTraining(`${_id}`);
+
+      if (status === "error") {
+        alert(message);
+        return;
+      }
+
+      setTrainings((prevState: TrainingInterface[]) => {
+        const newState = prevState.filter(e => e._id?.toString() !== _id);
+
+        return newState;
+      });
+
+      alert(message);
+    };
+
+    return (
+      <Training
+        key={_id}
+        id={`${_id}`}
+        date={parsedDate}
+        duration={duration}
+        exercises={exercises}
+        sets={numberOfSets}
+        handleDelete={handleDelete}
+      />
+    );
+  });
+
   return (
     <main className="trainings-container">
       <h1>Home</h1>
       <section className="trainings-section">
-        {trainings.map(({ _id, date, duration, exercises }) => {
-          const parsedDate = date.toLocaleDateString("es-ES", {
-            year: "numeric",
-            month: "short",
-            day: "numeric",
-          });
-
-          const numberOfSets = exercises.reduce((totalSets, exercise) => {
-            return totalSets + exercise.sets.length;
-          }, 0);
-
-          const handleDelete = async () => {
-            const { status, message } = await deleteTraining(`${_id}`);
-
-            if (status === "error") {
-              alert(message);
-              return;
-            }
-
-            setTrainings((prevState: TrainingInterface[]) => {
-              const newState = prevState.filter(e => e._id?.toString() !== _id);
-
-              return newState;
-            });
-
-            alert(message);
-          };
-
-          return (
-            <Training
-              key={_id}
-              id={`${_id}`}
-              date={parsedDate}
-              duration={duration}
-              exercises={exercises}
-              sets={numberOfSets}
-              handleDelete={handleDelete}
-            />
-          );
-        })}
+        {trainings.length !== 0 ? (
+          trainingsHTML
+        ) : (
+          <p>You haven&apos;t created a training!</p>
+        )}
       </section>
     </main>
   );
