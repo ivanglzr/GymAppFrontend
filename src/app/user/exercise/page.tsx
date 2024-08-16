2c876645c3e6b401ff1b719c3032ab135e7dd64a
Ivan <ivanglzr01@gmail.com>
2024-08-16 02:01:48 +0200

    UserExercisesPage is SSR and Exercises renders exercises correctly

--- src/app/user/exercise/page.tsx
+++ src/app/user/exercise/page.tsx
@@ -1,23 +1,8 @@
-"use client";
-
 import Aside from "@/components/Aside";
 import Container from "@/components/Container";
 import Exercises from "@/components/Exercises";
 
-import { useExercises } from "@/hooks/useExercises";
-
 export default function UserExercisesPage() {
-  const { exercises, error, loading } = useExercises();
-
-  if (error) {
-    return <h2>An error occurred</h2>;
-  }
-
-  //TODO: if user doesn't have any exercises loading lasts forever. Fix it
-  if (loading) {
-    return <h2>Loading...</h2>;
-  }
-
   return (
     <Container
       style={{
@@ -25,7 +10,7 @@ export default function UserExercisesPage() {
       }}
     >
       <Aside />
-      {exercises && <Exercises exercises={exercises} />}
+      <Exercises />
     </Container>
   );
 }
--- src/components/Exercises.tsx
+++ src/components/Exercises.tsx
@@ -1,19 +1,47 @@
+"use client";
+
 import "@/css/Exercises.css";
 
 import { UserExercise } from "../index.d";
 import Exercise from "./Exercise";
+import { useExercises } from "@/hooks/useExercises";
+import { useCallback } from "react";
 
-export default function Exercises({
-  exercises,
-}: {
-  exercises: Array<UserExercise>;
-}) {
+export default function Exercises() {
+  const { exercises, error, loading } = useExercises();
+
+  const exercisesHTML = useCallback(
+    () =>
+      exercises?.map((exercise: UserExercise) => (
+        <Exercise key={exercise._id} exercise={exercise} />
+      )),
+    [exercises]
+  );
+
+  if (error) {
+    return (
+      <main className="exercises-container">
+        <h2>An error occurred</h2>
+      </main>
+    );
+  }
+
+  if (loading) {
+    return (
+      <main className="exercises-container">
+        <h2>Loading...</h2>
+      </main>
+    );
+  }
+
   return (
     <main className="exercises-container">
       <h1>Exercises</h1>
-      {exercises.map((exercise: UserExercise) => (
-        <Exercise key={exercise._id} exercise={exercise} />
-      ))}
+      {exercises?.length === 0 ? (
+        <h2>User doesn&apos;t have any exercises</h2>
+      ) : (
+        exercisesHTML()
+      )}
     </main>
   );
 }
