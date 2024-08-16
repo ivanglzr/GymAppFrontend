9f3ee3ecb83a94ae2d3f612238790defb66cceba
Ivan <ivanglzr01@gmail.com>
2024-08-16 18:19:26 +0200

    Try Catch in all functions of the training service

--- src/services/training.ts
+++ src/services/training.ts
@@ -10,69 +10,114 @@ import { ROUTES } from "./constants";
 export async function getTraining(
   trainingId: string
 ): Promise<GetTrainingResponse> {
-  const petition = await fetch(ROUTES.URI + ROUTES.GET_TRAINING(trainingId), {
-    method: "GET",
-    credentials: "include",
-  });
-  const res = await petition.json();
+  try {
+    const petition = await fetch(ROUTES.URI + ROUTES.GET_TRAINING(trainingId), {
+      method: "GET",
+      credentials: "include",
+    });
 
-  return res;
+    if (!petition.ok) throw new Error(`HTTP Error! Status: ${petition.status}`);
+
+    const res: GetTrainingResponse = await petition.json();
+
+    if (res.status === "error") throw new Error(res.message);
+
+    return res;
+  } catch (error) {
+    throw new Error(`Failed to get training: ${error}`);
+  }
 }
 
 export async function getTrainings(): Promise<GetTrainingsResponse> {
-  const petition = await fetch(ROUTES.URI + ROUTES.GET_TRAININGS, {
-    method: "GET",
-    credentials: "include",
-  });
-  const res = await petition.json();
+  try {
+    const petition = await fetch(ROUTES.URI + ROUTES.GET_TRAININGS, {
+      method: "GET",
+      credentials: "include",
+    });
 
-  return res;
+    if (!petition.ok) throw new Error(`HTTP Error! Status: ${petition.status}`);
+
+    const res: GetTrainingsResponse = await petition.json();
+
+    if (res.status === "error") throw new Error(res.message);
+
+    return res;
+  } catch (error) {
+    throw new Error(`Failed to get trainings: ${error}`);
+  }
 }
 
 export async function postTraining(
   training: Training
 ): Promise<BackendResponse> {
-  const petition = await fetch(ROUTES.URI + ROUTES.POST_TRAINING, {
-    method: "POST",
-    credentials: "include",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(training),
-  });
-  const res = await petition.json();
+  try {
+    const petition = await fetch(ROUTES.URI + ROUTES.POST_TRAINING, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(training),
+    });
 
-  return res;
+    if (!petition.ok) throw new Error(`HTTP Error! Status: ${petition.status}`);
+
+    const res: BackendResponse = await petition.json();
+
+    if (res.status === "error") throw new Error(res.message);
+
+    return res;
+  } catch (error) {
+    throw new Error(`Failed to post training: ${error}`);
+  }
 }
 
 export async function putTraining(
   trainingId: string,
   training: Training
 ): Promise<BackendResponse> {
-  const petition = await fetch(ROUTES.URI + ROUTES.PUT_TRAINING(trainingId), {
-    method: "PUT",
-    credentials: "include",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(training),
-  });
-  const res = await petition.json();
+  try {
+    const petition = await fetch(ROUTES.URI + ROUTES.PUT_TRAINING(trainingId), {
+      method: "PUT",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(training),
+    });
 
-  return res;
+    if (!petition.ok) throw new Error(`HTTP Error! Status: ${petition.status}`);
+
+    const res: BackendResponse = await petition.json();
+
+    if (res.status === "error") throw new Error(res.message);
+
+    return res;
+  } catch (error) {
+    throw new Error(`Failed to post training: ${error}`);
+  }
 }
 
 export async function deleteTraining(
   trainingId: string
 ): Promise<BackendResponse> {
-  const petition = await fetch(
-    ROUTES.URI + ROUTES.DELETE_TRAINING(trainingId),
-    {
-      method: "DELETE",
-      credentials: "include",
-    }
-  );
-  const res = await petition.json();
+  try {
+    const petition = await fetch(
+      ROUTES.URI + ROUTES.DELETE_TRAINING(trainingId),
+      {
+        method: "DELETE",
+        credentials: "include",
+      }
+    );
 
-  return res;
+    if (!petition.ok) throw new Error(`HTTP Error! Status: ${petition.status}`);
+
+    const res: BackendResponse = await petition.json();
+
+    if (res.status === "error") throw new Error(res.message);
+
+    return res;
+  } catch (error) {
+    throw new Error(`Failed to delete training: ${error}`);
+  }
 }
