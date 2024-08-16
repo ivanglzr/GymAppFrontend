afda12c8746d793ba12250c98fd3d14585395174
Ivan <ivanglzr01@gmail.com>
2024-08-16 18:25:13 +0200

    Try Cath in all functions of the user service

--- src/services/user.ts
+++ src/services/user.ts
@@ -8,49 +8,86 @@ export async function login(
   email: string,
   password: string
 ): Promise<BackendResponse> {
-  const petition = await fetch(ROUTES.URI + ROUTES.LOGIN, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ email, password }),
-    credentials: "include",
-  });
-  const res = await petition.json();
+  try {
+    const petition = await fetch(ROUTES.URI + ROUTES.LOGIN, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+      credentials: "include",
+    });
 
-  return res;
+    if (!petition.ok) throw new Error(`HTTP Error! Status: ${petition.status}`);
+
+    const res: BackendResponse = await petition.json();
+
+    if (res.status === "error") throw new Error(res.message);
+
+    return res;
+  } catch (error) {
+    throw new Error(`Failed to login: ${error}`);
+  }
 }
 
 export async function logout(): Promise<BackendResponse> {
-  const petition = await fetch(ROUTES.URI + ROUTES.LOGOUT, {
-    method: "POST",
-    credentials: "include",
-  });
-  const res = await petition.json();
+  try {
+    const petition = await fetch(ROUTES.URI + ROUTES.LOGOUT, {
+      method: "POST",
+      credentials: "include",
+    });
 
-  return res;
+    if (!petition.ok) throw new Error(`HTTP Error! Status: ${petition.status}`);
+
+    const res = await petition.json();
+
+    if (res.status === "error") throw new Error(res.message);
+
+    return res;
+  } catch (error) {
+    throw new Error(`Failed to logout: ${error}`);
+  }
 }
 
 export async function getUser(): Promise<GetUserResponse> {
-  const petition = await fetch(ROUTES.URI + ROUTES.GET_USER, {
-    method: "GET",
-    credentials: "include",
-  });
-  const res = await petition.json();
+  try {
+    const petition = await fetch(ROUTES.URI + ROUTES.GET_USER, {
+      method: "GET",
+      credentials: "include",
+    });
 
-  return res;
+    if (!petition.ok) throw new Error(`HTTP Error! Status: ${petition.status}`);
+
+    const res = await petition.json();
+
+    if (res.status === "error") throw new Error(res.message);
+
+    return res;
+  } catch (error) {
+    throw new Error(`Failed to logout: ${error}`);
+  }
 }
 
 export async function postUser(user: User): Promise<BackendResponse> {
-  const petition = await fetch(ROUTES.URI + ROUTES.POST_USER, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(user),
-  });
+  try {
+    const petition = await fetch(ROUTES.URI + ROUTES.POST_USER, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
 
-  const res = await petition.json();
+    if (!petition.ok) throw new Error(`HTTP Error! Status: ${petition.status}`);
 
-  return res;
+    const res = await petition.json();
+
+    if (res.status === "error") throw new Error(res.message);
+
+    return res;
+  } catch (error) {
+    throw new Error(`Failed to logout: ${error}`);
+  }
 }
+
+//TODO: finish all methods
