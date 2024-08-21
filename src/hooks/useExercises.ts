c8b2404f344bee9548c7cbd9373efd36b7a6452d
Ivan <ivanglzr01@gmail.com>
2024-08-21 22:44:24 +0200

    searchExercise created

--- src/hooks/useExercises.ts
+++ src/hooks/useExercises.ts
@@ -3,14 +3,23 @@
 import { UserExercise } from "../index.d";
 import { GetUserExercisesResponse } from "@/interfaces/BackendResponses";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-import {
-  getUserExercises,
-  getUserExercisesBySearch,
-} from "@/services/exercise";
+import { getUserExercises } from "@/services/exercise";
+
+import debounce from "just-debounce-it";
+
+const filters = (exercise: UserExercise, search: string) =>
+  exercise.name.toString().toLowerCase().includes(search.toLowerCase()) ||
+  exercise.description
+    ?.toString()
+    .toLowerCase()
+    .includes(search.toLowerCase()) ||
+  exercise.muscle.toString().toLowerCase().includes(search.toLowerCase()) ||
+  exercise.equipment.toString().toLowerCase().includes(search.toLowerCase());
 
 export function useExercises() {
+  const originalExercises = useRef<Array<UserExercise>>();
   const [exercises, setExercises] = useState<Array<UserExercise>>();
   const [error, setError] = useState<boolean>(false);
   const loading = !exercises && !error;
@@ -18,20 +27,24 @@ export function useExercises() {
   useEffect(() => {
     getUserExercises()
       .then((res: GetUserExercisesResponse) => {
-        setExercises(res.exercises);
+        originalExercises.current = res.exercises;
+        setExercises(originalExercises.current);
       })
       .catch((_) => setError(true));
   }, []);
 
-  const searchExercises = async (search: string) => {
-    try {
-      const res = await getUserExercisesBySearch(search);
-
-      setExercises(res.exercises);
-    } catch (error) {
-      throw error;
+  const searchExercises = debounce(async (search: string) => {
+    if (!search || search.length === 0) {
+      setExercises(originalExercises.current);
+      return;
     }
-  };
+
+    const newExercises = originalExercises.current?.filter((exercise) =>
+      filters(exercise, search)
+    );
+
+    setExercises(newExercises);
+  }, 300);
 
   return { exercises, setExercises, searchExercises, error, loading };
 }
