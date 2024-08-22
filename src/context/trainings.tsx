e21a7a3cb694f4351093a9356132233b10cd060e
Ivan <ivanglzr01@gmail.com>
2024-08-23 00:11:05 +0200

    Trainings context created

---
+++ src/context/trainings.tsx
@@ -0,0 +1,33 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { Training } from "../index.d";
+import { getTrainings } from "@/services/training";
+
+type TrainingsContextType = {
+  trainings: Array<Training> | undefined;
+  error: boolean;
+  loading: boolean;
+};
+
+export const TrainingsContext = createContext<TrainingsContextType>({
+  trainings: undefined,
+  error: false,
+  loading: true,
+});
+
+export function TrainingsProvider({ children }: { children: ReactNode }) {
+  const [trainings, setTrainings] = useState<Array<Training>>();
+  const [error, setError] = useState<boolean>(false);
+  const loading = !trainings && !error;
+
+  useEffect(() => {
+    getTrainings()
+      .then((res) => setTrainings(res.trainings))
+      .catch((_) => setError(true));
+  }, []);
+
+  return (
+    <TrainingsContext.Provider value={{ trainings, error, loading }}>
+      {children}
+    </TrainingsContext.Provider>
+  );
+}
