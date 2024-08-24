a89f2ee0768a6a0dd522b4b821bfa35f650c6603
Ivan <ivanglzr01@gmail.com>
2024-08-24 16:21:33 +0200

    add update and delete icon to exercise

--- src/components/Exercise.tsx
+++ src/components/Exercise.tsx
@@ -1,15 +1,30 @@
 import { UserExercise } from "../index.d";
 
 import { getImageURL } from "@/utils/getImageURL";
+import { useRouter } from "next/navigation";
 
 export default function Exercise({ exercise }: { exercise: UserExercise }) {
+  const router = useRouter();
+
   const imageURL = getImageURL(exercise.image ?? "");
 
   return (
     <article className="exercise-article">
       <img src={imageURL} alt="img" className="exercise-img" />
       <div className="exercise-info">
-        <h2 className="subtitle exercise-info-title">{exercise.name}</h2>
+        <header className="exercise-header">
+          <h2 className="subtitle exercise-info-title">{exercise.name}</h2>
+          <div className="header-buttons">
+            <button
+              onClick={() => router.push(`/user/exercise/edit/${exercise._id}`)}
+            >
+              <i className="fa-solid fa-pen-to-square fa-xl"></i>
+            </button>
+            <button>
+              <i className="fa-solid fa-trash fa-xl"></i>
+            </button>
+          </div>
+        </header>
         <p className="exercise-info-description">{exercise.description}</p>
         <div className="exercise-data">
           <div className="exercise-data-div">
